3726343bfe97a38fa7f4fcf5ddb2be174fa703b5
grongierisc <guillaume.rongier@intersystems.com>
2020-08-10 15:48:22 +0200

    Add sql example + postman

---
+++ misc/sql/example.sql
@@ -0,0 +1,39 @@
+/* Select all Observation of Patient/1 */
+SELECT 
+*
+FROM HSFHIR_I0001_S.Observation
+where patient = 'Patient/1' 
+
+/* Select all Observation of Patient/1 with lonic code 718-7 (Hemoglobin [Mass/volume] in Blood)*/
+SELECT 
+*
+FROM HSFHIR_I0001_S.Observation
+where patient = 'Patient/1' and code [ '718-7'
+
+/* Get detail of the observation/16 */
+SELECT 
+*
+FROM HSFHIR_I0001_R.Rsrc where Key = 'Observation/16'
+
+/* Get valueQuantity of the observation/16 */
+SELECT 
+ID, Key, ResourceString, 
+GetJSON(ResourceString,'valueQuantity') as valueQuantity
+FROM HSFHIR_I0001_R.Rsrc where Key = 'Observation/16'
+
+/* Get value of valueQuantity of the observation/16 */
+SELECT 
+ID, Key, ResourceString, 
+GetJSON(ResourceString,'valueQuantity') as valueQuantity,
+GetProp(GetJSON(ResourceString,'valueQuantity'),'value') as value
+FROM HSFHIR_I0001_R.Rsrc where Key = 'Observation/16'
+
+/* An complexe example */
+SELECT 
+ID, Key, ResourceString, 
+GetJSON(ResourceString,'code') as code, 
+GetJSON(GetJSON(ResourceString,'code'),'coding') as coding,
+GetAtJSON(GetJSON(GetJSON(ResourceString,'code'),'coding'),1) as coding1,
+GetJSON(GetAtJSON(GetJSON(GetJSON(ResourceString,'code'),'coding'),1),'display') as display,
+GetProp(GetJSON(GetAtJSON(GetJSON(GetJSON(ResourceString,'code'),'coding'),1),'display'),'display')
+FROM HSFHIR_I0001_R.Rsrc where Key = 'Observation/16'
