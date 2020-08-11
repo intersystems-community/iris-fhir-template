2ccb3deb7081f38752a8ad4bb758bec9f8743b9c
grongierisc <guillaume.rongier@intersystems.com>
2020-08-11 12:05:30 +0200

    Enable JDBC connexion back to 2020.2

--- misc/sql/example.sql
+++ misc/sql/example.sql
@@ -2,31 +2,31 @@
 SELECT 
 *
 FROM HSFHIR_I0001_S.Observation
-where patient = 'Patient/1' 
+where patient = 'Patient/3' ;
 
 /* Select all Observation of Patient/1 with lonic code 718-7 (Hemoglobin [Mass/volume] in Blood)*/
 SELECT 
 *
 FROM HSFHIR_I0001_S.Observation
-where patient = 'Patient/1' and code [ '718-7'
+where patient = 'Patient/3' and code [ '718-7';
 
 /* Get detail of the observation/16 */
 SELECT 
 *
-FROM HSFHIR_I0001_R.Rsrc where Key = 'Observation/16'
+FROM HSFHIR_I0001_R.Rsrc where Key = 'Observation/31';
 
 /* Get valueQuantity of the observation/16 */
 SELECT 
 ID, Key, ResourceString, 
 GetJSON(ResourceString,'valueQuantity') as valueQuantity
-FROM HSFHIR_I0001_R.Rsrc where Key = 'Observation/16'
+FROM HSFHIR_I0001_R.Rsrc where Key = 'Observation/31';
 
 /* Get value of valueQuantity of the observation/16 */
 SELECT 
 ID, Key, ResourceString, 
 GetJSON(ResourceString,'valueQuantity') as valueQuantity,
 GetProp(GetJSON(ResourceString,'valueQuantity'),'value') as value
-FROM HSFHIR_I0001_R.Rsrc where Key = 'Observation/16'
+FROM HSFHIR_I0001_R.Rsrc where Key = 'Observation/31';
 
 /* An complexe example */
 SELECT 
@@ -35,5 +35,5 @@ GetJSON(ResourceString,'code') as code,
 GetJSON(GetJSON(ResourceString,'code'),'coding') as coding,
 GetAtJSON(GetJSON(GetJSON(ResourceString,'code'),'coding'),0) as coding1,
 GetJSON(GetAtJSON(GetJSON(GetJSON(ResourceString,'code'),'coding'),0),'display') as display,
-GetProp(GetJSON(GetAtJSON(GetJSON(GetJSON(ResourceString,'code'),'coding'),0),'display'),'display')
-FROM HSFHIR_I0001_R.Rsrc where Key = 'Observation/16'
+GetProp(GetJSON(GetAtJSON(GetJSON(GetJSON(ResourceString,'code'),'coding'),0),'display'),'display') as value
+FROM HSFHIR_I0001_R.Rsrc where Key = 'Observation/31';
