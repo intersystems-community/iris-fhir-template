c3b08881dd0417dec760fdb64321dc8d7b991830
evshvarov <evgeny.shvarov@gmail.com>
2020-08-16 11:06:22 +0300

    output fix

--- synthea-loader.bat
+++ synthea-loader.bat
@@ -4,4 +4,4 @@ if not "%1"=="" (
     set numberOfPatients=%1
 )
 
-docker run --rm -v %~dp0\data:/output --name synthea-docker intersystemsdc/irisdemo-base-synthea:version-1.3.4 -p %numberOfPatients%
+docker run --rm -v %~dp0\output:/output --name synthea-docker intersystemsdc/irisdemo-base-synthea:version-1.3.4 -p %numberOfPatients%
--- synthea-loader.sh
+++ synthea-loader.sh
@@ -10,5 +10,5 @@ else
     #numberOfPatients=10
 fi
 
-docker run --rm -v $PWD/data:/output --name synthea-docker intersystemsdc/irisdemo-base-synthea:version-1.3.4 -p $numberOfPatients
+docker run --rm -v $PWD/output:/output --name synthea-docker intersystemsdc/irisdemo-base-synthea:version-1.3.4 -p $numberOfPatients
 
